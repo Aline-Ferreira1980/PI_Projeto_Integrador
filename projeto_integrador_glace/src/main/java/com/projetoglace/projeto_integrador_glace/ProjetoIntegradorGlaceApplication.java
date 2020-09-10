de5d5a2727b01bdc201dfbb2b316f7c635292678
Aline-Ferreira1980 <aline.fn80@gmail.com>
2020-09-10 16:36:02 -0300

    projeto start

---
+++ projeto_integrador_glace/src/main/java/com/projetoglace/projeto_integrador_glace/ProjetoIntegradorGlaceApplication.java
@@ -0,0 +1,13 @@
+package com.projetoglace.projeto_integrador_glace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorGlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorGlaceApplication.class, args);
+	}
+
+}
---
+++ projeto_integrador_glace/src/test/java/com/projetoglace/projeto_integrador_glace/ProjetoIntegradorGlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoglace.projeto_integrador_glace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorGlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
